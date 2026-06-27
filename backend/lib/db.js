2ab7dcab90ae2c651f5c3e5f9597f27043b60254
rku-21 <rahul.k.upadhyay21@gmail.com>
2026-06-27 10:54:57 +0530

    fix database connection issue

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -2,12 +2,14 @@ import mongoose  from "mongoose";
 
 export const connectDB = async () => {
     try {
+        console.log("MONGO URI EXISTS:", !!process.env.MONGODB_URI);
         const conn = await mongoose.connect(process.env.MONGODB_URI, {
             serverSelectionTimeoutMS: 5000,
         });
         console.log(`MongoDB connected: ${conn.connection.host}`);
         return conn;
     } catch(error) {
-        console.log(error);
-    }
+    console.error("MongoDB Error:", error);
+    throw error;
+}
 }
